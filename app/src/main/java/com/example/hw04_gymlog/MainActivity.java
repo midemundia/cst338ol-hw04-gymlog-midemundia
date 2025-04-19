525826e9a1093b4871b478cc7dcd48eb9a0cddef
midemundia <matthias.idemundia@gmail.com>
2025-04-18 21:05:12 -0700

    Connect input fields and button action.

--- app/src/main/java/com/example/hw04_gymlog/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog/MainActivity.java
@@ -1,24 +1,64 @@
+/**
+ * Name: Matthias Idemundia
+ * Date: 04/16/2025
+ * Explanation: HW04 Gymlog Class
+ */
+
 package com.example.hw04_gymlog;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.hw04_gymlog.databinding.ActivityMainBinding;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private static final String TAG = "GYMLOG";
+    String mExercise = "";
+    double mWeight = 0.0;
+    int mReps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateDisplay();
+                getInformationFromDisplay();
+            }
         });
     }
+
+    private void updateDisplay() {
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        String newDisplay = String.format(Locale.US,"Exercise: %s%nWeight: %f%nReps: %d%n=-=-=%n%s", mExercise, mWeight, mReps, currentInfo);
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+
+    private void getInformationFromDisplay(){
+        mExercise = binding.exerciseInputEditText.getText().toString();
+        try {
+            mWeight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error while reading weight input text.");
+        }
+
+        try {
+            mReps = Integer.parseInt(binding.repInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error while reading rep input text.");
+        }
+    }
+
 }
